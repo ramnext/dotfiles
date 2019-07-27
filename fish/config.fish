0f20512facd84b96e8eb5edee99ec0da4a2d1c72
ramnext <ramnextevolutions@gmail.com>
2019-07-27 23:22:10 +0900

    user key bindings 見直し

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,6 @@ if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fisher
 end
-set fish_plugins theme peco
 
 function fish_user_key_bindings
 
@@ -18,6 +17,13 @@ function fish_user_key_bindings
     bind -M default -e \ce
     bind -M insert -e \cf
     bind -M insert \ce accept-autosuggestion
+
+    # change word move keybindings
+    bind -M insert \cb backward-word
+    bind -M insert \cf forward-word
+    bind -M insert \ef forward-char
+    bind -M insert \eb backward-char
+
 
     ### fzf ###
     set -q FZF_LEGACY_KEYBINDINGS
@@ -34,12 +40,12 @@ function fish_user_key_bindings
             bind -M insert \eC '__fzf_cd --hidden'
         end
     else
-        bind \cf '__fzf_find_file'
+        bind \cs '__fzf_find_file'
         bind \cr '__fzf_reverse_isearch'
         bind \eo '__fzf_cd'
         bind \eO '__fzf_cd --hidden'
         if bind -M insert >/dev/null 2>/dev/null
-            bind -M insert \cf '__fzf_find_file'
+            bind -M insert \cs '__fzf_find_file'
             bind -M insert \cr '__fzf_reverse_isearch'
             bind -M insert \eo '__fzf_cd'
             bind -M insert \eO '__fzf_cd --hidden'
@@ -49,37 +55,42 @@ function fish_user_key_bindings
         bind \t '__fzf_complete'
     end
     ### fzf ###
-    ### ghq ###
-    bind \cg '__ghq_crtl_g'
-    if bind -M insert >/dev/null ^/dev/null
-        bind -M insert \cg '__ghq_crtl_g'
-    end
-    ### ghq ###
 
-    # Bind for prco history to Ctrl+h
-    bind \cs 'peco_select_history (commandline -b)'
-    # Bind for prco Recent history to Ctrl+g
-    bind \ch 'peco_recentd'
-    # Bidn for ghq history to Ctrl+]
-    bind \c] peco_select_ghq_repository # ghq用のリポジトリ候補表示
-    # Bidn for gh-open history to Ctrl+o
-    bind \co peco_open_gh_repository # gh-openの呼び出し
+    ### ghq ###
+    bind \cg '__ghq_repository_search'
     if bind -M insert >/dev/null 2>/dev/null
-        bind -M insert \cs 'peco_select_history (commandline -b)'
-        bind -M insert \ch 'peco_recentd'
-        bind -M insert \c] 'peco_select_ghq_repository' # ghq用のリポジトリ候補表示
-        bind -M insert \co 'peco_open_gh_repository'    # gh-openの呼び出し
+        bind -M insert \cg '__ghq_repository_search'
     end
+
+#@    # Bind for prco history to Ctrl+h
+#@    bind \cs 'peco_select_history (commandline -b)'
+#@    # Bind for prco Recent history to Ctrl+g
+#@    bind \ch 'peco_recentd'
+#@    # Bidn for ghq history to Ctrl+]
+#@    bind \c] peco_select_ghq_repository # ghq用のリポジトリ候補表示
+#@    # Bidn for gh-open history to Ctrl+o
+#@    bind \co peco_open_gh_repository # gh-openの呼び出し
+#@    if bind -M insert >/dev/null 2>/dev/null
+#@        bind -M insert \cs 'peco_select_history (commandline -b)'
+#@        bind -M insert \ch 'peco_recentd'
+#@        bind -M insert \c] 'peco_select_ghq_repository' # ghq用のリポジトリ候補表示
+#@        bind -M insert \co 'peco_open_gh_repository'    # gh-openの呼び出し
+#@    end
 end
 
 ###
-### pyenv
+### anyenv
 ###
-#source (conda info --root)/etc/fish/conf.d/conda.fish
-set PATH $HOME/.anyenv/envs/pyenv/shims $PATH
-eval (pyenv init - | source)
+status --is-interactive; and source (anyenv init -|psub)
 
 ###
-### SDKMAN
+### sqlite
 ###
-sdk help > /dev/null
+set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
+##
+## sqlite for COMPILE
+##
+set -gx LDFLAGS "-L/usr/local/opt/sqlite/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/sqlite/include"
+
+set -gx PKG_CONFIG_PATH "/usr/local/opt/sqlite/lib/pkgconfig"
