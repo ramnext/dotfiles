f7b5ee9b199b75a201e5eebbc72c0b4ebfc3a875
SUZUKI Toshio <toshio.suzuki@ctc-g.co.jp>
2019-12-06 14:18:18 +0900

    Add ls after cd function.

---
+++ fish/functions/cdl.fish
@@ -0,0 +1,35 @@
+function cdl
+    if test (count $argv) -eq 0
+        return 0
+    else if test (count $argv) -gt 1
+        printf "%s\n" (_ "Too many args for cd command")
+        return 1
+    end
+    # Avoid set completions.
+    set -l previous $PWD
+
+    if test "$argv" = "-"
+        if test "$__fish_cd_direction" = "next"
+            nextd
+        else
+            prevd
+        end
+        return $status
+    end
+    builtin cd $argv
+    set -l cd_status $status
+    # Log history
+    if test $cd_status -eq 0 -a "$PWD" != "$previous"
+        set -q dirprev[$MAX_DIR_HIST]
+        and set -e dirprev[1]
+        set -g dirprev $dirprev $previous
+        set -e dirnext
+        set -g __fish_cd_direction prev
+    end
+
+    if test $cd_status -ne 0
+        return 1
+    end
+    exa -lgF --group-directories-first --icons
+    return $status
+end
