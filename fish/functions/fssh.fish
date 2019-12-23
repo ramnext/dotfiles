f57877b32957145b0eb33ca523569eebeaa44bd8
Toshio SUZUKI <toshio.suzuki@ctc-g.co.jp>
2019-12-24 06:11:34 +0900

    Modify fssh use rg command option. rg command add smart-case option.

--- fish/functions/fssh.fish
+++ fish/functions/fssh.fish
@@ -1,4 +1,4 @@
 function fssh -d "Fuzzy-find ssh host via ag and ssh into it"
-  rg '^host [^*]' ~/.ssh/config | cut -d ' ' -f 2 | fzf | read -l result; and ssh "$result"
+  rg -S '^host [^*]' ~/.ssh/config | cut -d ' ' -f 2 | fzf | read -l result; and ssh "$result"
 end
 
