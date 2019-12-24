288b39a0d8e2d17d943ff97607f98261a6c23e6d
ramnext <ramnextevolutions@gmail.com>
2019-12-24 23:13:25 +0900

    Add git branch preview option.

--- fish/functions/fco.fish
+++ fish/functions/fco.fish
@@ -1,4 +1,4 @@
 function fco -d "Fuzzy-find and checkout a branch"
-  git branch --all | grep -v HEAD | string trim | fzf | read -l result; and git checkout "$result"
+  git branch --all | grep -v HEAD | string trim | fzf --preview 'git show --color=always {}' | read -l result; and git checkout "$result"
 end
 
