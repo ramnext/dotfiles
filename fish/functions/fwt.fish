d3624e3dd8535e501d1cb3046eed265e2201a4c2
ramnext <ramnextevolutions@gmail.com>
2019-12-26 01:17:54 +0900

    Bug fix: fwt command not work Ctrl+C or Ctrl+G.

--- fish/functions/fwt.fish
+++ fish/functions/fwt.fish
@@ -1,16 +1,14 @@
 function fwt -d "Fuzzy-find and move worktree directory"
-    git rev-parse > /dev/null ^&1
-    if test $status -ne 0
-        echo 'fatal: Not a git repository.'
-        exit
-    end
+  git rev-parse > /dev/null ^&1
+  if test $status -ne 0
+    echo 'fatal: Not a git repository.'
+    return
+  end
 
-    set selectedWorkTreeDir (git worktree list | fzf | awk '{print $1}')
+  set -l selectedWorkTreeDir
+  git worktree list | fzf | awk '{print $1}' | read select
 
-    if test $selectedWorkTreeDir = ""
-        exit
-    end
-
-    cd $selectedWorkTreeDir
+  [ -n "$select" ]; and cd "$select"
+  commandline -f repaint
 end
 
