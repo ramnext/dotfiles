6193210f642bb3977057e449e283d465944ea416
Toshio SUZUKI <toshio.suzuki@ctc-g.co.jp>
2020-01-06 18:40:18 +0900

    Modify anyenv init parameter for compatible fish.

--- fish/config.fish
+++ fish/config.fish
@@ -69,7 +69,7 @@ set -x GHQ_ROOT $HOME/repos
 ## anyenv
 ##
 status is-login; and set -x fish_user_paths $HOME/.anyenv/bin $fish_user_paths
-status --is-interactive; and source (anyenv init -|psub)
+status --is-interactive; and source (anyenv init - fish|psub)
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
