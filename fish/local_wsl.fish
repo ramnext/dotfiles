88a4c1f58c405689784bde343d2f025c0fd1b484
Toshio SUZUKI <toshio.suzuki@ctc-g.co.jp>
2020-04-24 10:34:37 +0900

    Update proxy password.

--- fish/local_wsl.fish
+++ fish/local_wsl.fish
@@ -1,16 +1,15 @@
 # proxy setting
-set -x http_proxy 'http://z1h6022:ki__CP+9@ctcpro.ctc-g.co.jp:8080'
+set -x http_proxy 'http://z1h6022:ki__CP+0@ctcpro.ctc-g.co.jp:8080'
 set -x https_proxy $http_proxy
 set -x no_proxy '127.0.0.1,localhost,10.213.194.96,10.213.194.105,10.213.194.107'
 set -x http_proxy_user 'z1h6022'
-set -x http_proxy_pass 'ki__CP+9'
+set -x http_proxy_pass 'ki__CP+0'
 
 # docker setting
 set -x DOCKER_HOST 'tcp://localhost:2375'
 
 # for Win10 17.09. Win10 18.03 later can delete
-status --is-login; and sudo mount --bind /mnt/c /c
+#del status --is-login; and sudo mount --bind /mnt/c /c
 
 ## for neovim
 alias vi='~/squashfs-root/usr/bin/nvim'
-
