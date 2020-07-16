8ceaa32dc3c63bd74fe7edd0d44b00b642c1f97f
Toshio SUZUKI <toshio.suzuki@ctc-g.co.jp>
2020-07-16 11:27:29 +0900

    Update proxy password.

--- fish/local_wsl.fish
+++ fish/local_wsl.fish
@@ -1,7 +1,7 @@
 # proxy setting
 set -x http_proxy 'http://z1h6022:ki__CP+1@ctcpro.ctc-g.co.jp:8080'
 set -x https_proxy $http_proxy
-set -x no_proxy '127.0.0.1,localhost,10.213.194.96,10.213.194.105,10.213.194.107'
+set -x no_proxy '127.0.0.1,localhost,10.213.194.96,10.213.194.105,10.213.194.107,192.168.65.*'
 set -x http_proxy_user 'z1h6022'
 set -x http_proxy_pass 'ki__CP+1'
 
